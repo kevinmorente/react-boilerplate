f40d3b9becd1ca42b3f55be8b96675e5276df725
Kevin Morente <morente.kevin@gmail.com>
2024-01-16 11:13:31 -0300

    Start-RevA

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,10 @@
+import { MyButton } from './styles'
+
+function Button(){
+    return(
+        <div>
+            <MyButton>Click em mim!</MyButton>
+        </div>
+    )
+}
+export default Button
